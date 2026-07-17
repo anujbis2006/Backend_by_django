ad1e6067f336efcfb8f2cb5fbcef7c0e8378d70f
anujbis2006 <anujbiswas303@gmail.com>
2026-07-17 22:16:28 +0530

    Route root to blog app (fix 404 at /)

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -21,6 +21,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
 ]
